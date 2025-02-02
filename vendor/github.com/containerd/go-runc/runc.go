15ee00967d637939e8fcd4467b8ec02ad9f266f7
hanzbzz <honza.brazda2@seznam.cz>
2025-02-02 12:18:52 +0100

    write criu args to a file for debug

--- vendor/github.com/containerd/go-runc/runc.go
+++ vendor/github.com/containerd/go-runc/runc.go
@@ -611,6 +611,20 @@ func (r *Runc) Checkpoint(context context.Context, id string, opts *CheckpointOp
 	for _, a := range actions {
 		args = a(args)
 	}
+	// Open file for writing
+	file, err := os.Create("/tmp/criu-debug.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Write args to file
+	for _, str := range args {
+		_, err := io.WriteString(file, str+" ")
+		if err != nil {
+			panic(err)
+		}
+	}
 	cmd := r.command(context, append(args, id)...)
 	cmd.ExtraFiles = extraFiles
 	return r.runOrError(cmd)
